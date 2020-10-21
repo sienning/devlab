c8b83a0151eda93db7485ba39729c9be01d59c65
Léna ENG <55486985+sienning@users.noreply.github.com>
2020-10-21 11:19:26 +0200

    Ajouts barres

--- tidmarsh-app/src/components/Article1.js
+++ tidmarsh-app/src/components/Article1.js
@@ -1,32 +1,7 @@
 import React, { useState } from 'react';
-import { Grid, Image, Modal } from 'semantic-ui-react';
+import { Grid, Image } from 'semantic-ui-react';
 // import Chargement from './Chargement';
 import ModalArticle from './ModalArticle';
-
-// const ModalArticle = ({ openArticle }) => {
-//     const [open, setOpen] = useState(false)
-
-//     return (
-//         <Modal
-//             closeIcon
-//             open={open}
-//             onClose={() => setOpen(false)}
-//             onOpen={() => setOpen(true)}
-//         >
-//             {console.log("openArticle", openArticle)}
-//             {console.log("open", open)}
-//             <Header icon='archive' content='Archive Old Messages' />
-//             <Modal.Content>
-//                 <p>
-//                     Your inbox is getting full, would you like us to enable automatic
-//                     archiving of old messages?
-//                 </p>
-//             </Modal.Content>
-            
-//         </Modal>
-//     );
-// }
-
 
 const Article1 = () => {
     const [texte1, setTexte1] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
@@ -34,7 +9,7 @@ const Article1 = () => {
     return(
         <div>
             <Grid columns={3} divided>
-                <Grid.Column> EX1 : Jeu concours </Grid.Column>
+                <Grid.Column> <span>Jeu concours :</span> À gagner +3% d’humidité sur la zone humide est de Tidmarsh soit [mettre valeur exacte par rapport au data, faire évoluer par rapport à la date] </Grid.Column>
                 <Grid.Column> EX2 : Météo </Grid.Column>
                 <Grid.Column> EX3 : Signe astrologique </Grid.Column>
             </Grid>
@@ -49,12 +24,13 @@ const Article1 = () => {
                 <Grid.Column width={2}>
                     <span className="titre-lettre">P</span>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt utLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
+                    <Image src="./images/barre-2-HP.jpg" />
+                
                 </Grid.Column>
                 <Grid.Column width={3}>
                     
                     <ModalArticle texteArticle={texte1} />
-          
-          
+                    <Image src="./images/barre-HP.jpg" />
                 </Grid.Column>
                 <Grid.Column width={3}>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -63,6 +39,8 @@ const Article1 = () => {
                 </Grid.Column>
                 <Grid.Column width={8}>
                     <div><Image src="./images/zh-nb.png"/></div>
+                    <Image src="./images/barre-3-HP.jpg" style={{ width : "100%", marginTop : "3px" }} />
+
                 </Grid.Column>
             </Grid>
             <Grid>
--- tidmarsh-app/src/components/ModalArticle.js
+++ tidmarsh-app/src/components/ModalArticle.js
@@ -1,28 +1,27 @@
 import React, { useState, useEffect } from 'react'
 import { Header, Modal } from 'semantic-ui-react'
 
-function ModalArticle({ openArticle, texteArticle }) {
+function ModalArticle({ texteArticle }) {
     const [open, setOpen] = useState(false)
 
     return (
-        <Modal
-            closeIcon
-            open={open}
-            onClose={() => setOpen(false)}
-            onOpen={() => setOpen(true)}
-            trigger={<p className="article">{texteArticle}</p>}
-        >
-            {console.log("openArticle", openArticle)}
-            {console.log("open", open)}
-            <Header icon='archive' content='Archive Old Messages' />
-            <Modal.Content>
-                <p>
-                    Your inbox is getting full, would you like us to enable automatic
-                    archiving of old messages?
-                </p>
-            </Modal.Content>
-            
-        </Modal>
+        <div>
+            <Modal
+                closeIcon
+                open={open}
+                onClose={() => setOpen(false)}
+                onOpen={() => setOpen(true)}
+                trigger={<div className="article"><p>{texteArticle}</p></div>}
+            >
+                <Header icon='archive' content="Définition d'une zone humide" />
+                <Modal.Content>
+                    <p>
+                        Hello
+                    </p>
+                </Modal.Content>
+                
+            </Modal>
+        </div>
     );
 }
 
