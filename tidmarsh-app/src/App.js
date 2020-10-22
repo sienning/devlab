9fd6ea28100a902cdadf3426a97d41a7c6903c84
HugoPerard <48803115+HugoPerard@users.noreply.github.com>
2020-10-23 01:08:19 +0200

    add humidityTemperatureChart

--- tidmarsh-app/src/App.js
+++ tidmarsh-app/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
-import Tableau1 from './components/Tableau1';
 import { Container } from 'semantic-ui-react';
+import Tableau1 from './components/Tableau1';
 
 function App() {
   return (
     <div className="App">
-      <Container> 
+      <Container>
           <Tableau1/>
       </Container>
     </div>
