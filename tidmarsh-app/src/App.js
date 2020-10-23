01347a7ad17e90ce81d74cdbdfc133d01905b4ea
HugoPerard <48803115+HugoPerard@users.noreply.github.com>
2020-10-23 11:15:46 +0200

    add whiteLightChart

--- tidmarsh-app/src/App.js
+++ tidmarsh-app/src/App.js
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react';
 import Tableau1 from './components/Tableau1';
 import { HumidityTemperatureChart } from './components/HumidityTemperatureChart';
+import { WhiteLightChart } from './components/WhiteLightChart';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Container>
           <Tableau1/>
           <HumidityTemperatureChart/>
+          <WhiteLightChart/>
       </Container>
     </div>
   );
