cef61ac58667291ec774e2067efb7fb590800b2c
Hugo Pérard <hugo.perard@bearstudio.fr>
2021-04-30 11:57:54 +0200

    rework charts api call, in charts.service, with react query

--- tidmarsh-app/src/App.js
+++ tidmarsh-app/src/App.js
@@ -4,19 +4,27 @@ import "semantic-ui-css/semantic.min.css";
 import { Container } from "semantic-ui-react";
 import PageArticle from "./articles/PageArticle";
 import Article1 from "./articles/Article1";
+import { QueryClient, QueryClientProvider } from "react-query";
+import axios from "axios";
+
 // import { WhiteLightChart } from './components/WhiteLightChart';
+const queryClient = new QueryClient();
+
+axios.defaults.baseURL = "https://chain-api.media.mit.edu";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <PageArticle>
-          {/* <Article2 isLoading={isLoading /> */}
-          <Article1 />
-        </PageArticle>
-        {/* <HumidityTemperatureChart/> */}
-        {/* <WhiteLightChart/> */}
-      </Container>
+      <QueryClientProvider client={queryClient}>
+        <Container>
+          <PageArticle>
+            {/* <Article2 isLoading={isLoading /> */}
+            <Article1 />
+          </PageArticle>
+          {/* <HumidityTemperatureChart/> */}
+          {/* <WhiteLightChart/> */}
+        </Container>
+      </QueryClientProvider>
     </div>
   );
 }
--- tidmarsh-app/src/charts/WhiteLightChart.js
+++ tidmarsh-app/src/charts/WhiteLightChart.js
@@ -1,79 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import dayjs from 'dayjs';
-import { Chart } from 'react-charts';
+import React from "react";
+import { Chart } from "react-charts";
+import { useWhiteLightData } from "../charts/charts.service";
+import Chargement from "../components/Chargement";
 
 const WhiteLightChart = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [whiteLightDatas, setWhiteLightDatas] = useState([]);
+  const { data: whiteLight, isLoading } = useWhiteLightData();
 
-    const yesterday = dayjs().subtract(1, 'day').format('YYYY-MM-DD').toString();
+  const data = React.useMemo(
+    () => [
+      {
+        label: "Humidité",
+        data: whiteLight?.data,
+        color: "#696969",
+      },
+    ],
+    [whiteLight]
+  );
 
-    const getWhiteLightDatas = async () => {
-        setIsLoading(true);
-        await axios.get('https://chain-api.media.mit.edu/aggregate_data/?sensor_id=9913&aggtime=1h')
-            .then((response) => {
-                setWhiteLightDataFilterOneDay(response.data.data);
-                setIsLoading(false);
-            })
-            .catch((error) => { console.log(error) });
-    };
+  const series = React.useMemo(
+    () => ({
+      type: "bar",
+    }),
+    []
+  );
 
-    const setWhiteLightDataFilterOneDay = datas => {
-        const result = [];
-        for (let i = 0; i < 24; ++i) {
-            const toFound = yesterday + 'T' + (i.toString().length === 1 ? '0' + i.toString() : i.toString());
-            result.push({ x: i, y: datas.find(data => data.timestamp.includes(toFound)).mean});
-        }
-        setWhiteLightDatas(result);
-    };
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: "utc", position: "bottom" },
+      { type: "linear", position: "left" },
+    ],
+    []
+  );
 
-    useEffect(() => {
-        getWhiteLightDatas();
-    }, []);
-
-    const data = React.useMemo(
-        () => [
-            {
-                label: 'Humidité',
-                data: whiteLightDatas,
-                color: '#696969'
-            },
-        ],
-        [whiteLightDatas]
-    )
-
-    const series = React.useMemo(
-        () => ({
-            type: 'bar'
-        }),
-        []
-    )
-
-    const axes = React.useMemo(
-        () => [
-            { primary: true, type: 'utc', position: 'bottom' },
-            { type: 'linear', position: 'left' },
-        ],
-        []
-    )
-
-    return (
-        <div
-            style={{
-                marginTop: "20px",
-                width: '400px',
-                height: '300px',
-                display: 'flex',
-                fontWeight: 'bold',
-            }}
-        >
-            <p className="ui label" style={{ fontFamily: 'Fredericka the Great', writingMode: 'vertical-rl', textOrientation: 'upright', textAlign: 'center', backgroundColor: '#696969', color: '#ece8dc'}}>
-                Lumière blanche
-            </p>
-            <Chart data={data} axes={axes} series={series}/>
-        </div>
-    )
-}
+  return (
+    <div
+      style={{
+        marginTop: "20px",
+        width: "400px",
+        height: "300px",
+        display: "flex",
+        fontWeight: "bold",
+      }}
+    >
+      <p
+        className="ui label"
+        style={{
+          fontFamily: "Fredericka the Great",
+          writingMode: "vertical-rl",
+          textOrientation: "upright",
+          textAlign: "center",
+          backgroundColor: "#696969",
+          color: "#ece8dc",
+        }}
+      >
+        Lumière blanche
+      </p>
+      {isLoading ? (
+        <Chargement />
+      ) : (
+        <Chart data={data} axes={axes} series={series} />
+      )}
+    </div>
+  );
+};
 
 export default WhiteLightChart;
